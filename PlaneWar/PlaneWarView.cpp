f6c51293fbaf28c0681cd6c7cd5216abe58e8af5
XULINK <xulinxiao@live.com>
2016-07-04 16:17:12 +0800

    添加项目文件。

---
+++ PlaneWar/PlaneWarView.cpp
@@ -0,0 +1,96 @@
+
+// PlaneWarView.cpp : CPlaneWarView 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "PlaneWar.h"
+#endif
+
+#include "PlaneWarDoc.h"
+#include "PlaneWarView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CPlaneWarView
+
+IMPLEMENT_DYNCREATE(CPlaneWarView, CView)
+
+BEGIN_MESSAGE_MAP(CPlaneWarView, CView)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CPlaneWarView 构造/析构
+
+CPlaneWarView::CPlaneWarView()
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CPlaneWarView::~CPlaneWarView()
+{
+}
+
+BOOL CPlaneWarView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CPlaneWarView 绘制
+
+void CPlaneWarView::OnDraw(CDC* /*pDC*/)
+{
+	CPlaneWarDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+void CPlaneWarView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CPlaneWarView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CPlaneWarView 诊断
+
+#ifdef _DEBUG
+void CPlaneWarView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CPlaneWarView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CPlaneWarDoc* CPlaneWarView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CPlaneWarDoc)));
+	return (CPlaneWarDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CPlaneWarView 消息处理程序
---
+++ PlaneWar/PlaneWarView.h
@@ -0,0 +1,49 @@
+
+// PlaneWarView.h : CPlaneWarView 类的接口
+//
+
+#pragma once
+
+
+class CPlaneWarView : public CView
+{
+protected: // 仅从序列化创建
+	CPlaneWarView();
+	DECLARE_DYNCREATE(CPlaneWarView)
+
+// 特性
+public:
+	CPlaneWarDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 实现
+public:
+	virtual ~CPlaneWarView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // PlaneWarView.cpp 中的调试版本
+inline CPlaneWarDoc* CPlaneWarView::GetDocument() const
+   { return reinterpret_cast<CPlaneWarDoc*>(m_pDocument); }
+#endif
+
